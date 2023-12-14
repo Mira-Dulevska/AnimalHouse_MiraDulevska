775389f2a34314b35b7ea9f259dee7cab211f239
Mira-Dulevska <mirad456@abv.bg>
2023-12-14 15:12:50 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    AnimalHouse<Tiger> tigerHouse = new AnimalHouse<Tiger>();
+        AnimalHouse<Bird> birdHouse = new AnimalHouse<Bird>();
+
+        Tiger tiger = new Tiger();
+        Bird bird = new Bird();
+
+        tigerHouse.animal = tiger;
+        birdHouse.animal = bird;
+    }
+}
---
+++ src/com/company/Tiger.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Tiger extends Animal{
+    public String name;
+}
